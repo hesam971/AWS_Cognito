b340aeceb22aef4e76333f271f8fa491cdcbbbd6
hesam971 <56349036+hesam971@users.noreply.github.com>
2024-07-27 23:52:06 +0200

    Update example.spec.js

--- my-aws-app/my-app/tests/example.spec.js
+++ my-aws-app/my-app/tests/example.spec.js
@@ -1,19 +1,34 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Signup' }).click();
+  await page.locator('div').filter({ hasText: /^Username$/ }).getByRole('textbox').click();
+  await page.locator('div').filter({ hasText: /^Username$/ }).getByRole('textbox').fill('hesam');
+  await page.locator('input[type="email"]').click();
+  await page.locator('input[type="email"]').fill('hesam971@gmail.com');
+  await page.locator('input[type="password"]').click();
+  await page.locator('input[type="password"]').fill('123456abcd');
+  await page.locator('div').filter({ hasText: /^Favorite Sport or Team$/ }).getByRole('textbox').click();
+  await page.locator('div').filter({ hasText: /^Favorite Sport or Team$/ }).getByRole('textbox').fill('liverpool');
+  await page.getByRole('button', { name: 'Sign Up' }).click();
+  await page.locator('div').filter({ hasText: /^Username$/ }).getByRole('textbox').click();
+  await page.locator('div').filter({ hasText: /^Username$/ }).getByRole('textbox').fill('test');
+  await page.locator('input[type="email"]').click();
+  await page.locator('input[type="email"]').fill('test@gmail.com');
+  await page.locator('input[type="password"]').click();
+  await page.locator('input[type="password"]').fill('123456abcd');
+  await page.locator('div').filter({ hasText: /^Favorite Sport or Team$/ }).getByRole('textbox').click();
+  await page.locator('div').filter({ hasText: /^Favorite Sport or Team$/ }).getByRole('textbox').fill('arsenal');
+  await page.getByRole('button', { name: 'Sign Up' }).click();
+  await page.locator('input[type="email"]').fill('test1@gmail.com');
+  await page.locator('input[type="password"]').click();
+  await page.locator('input[type="password"]').fill('123456abc');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.locator('input[type="email"]').click();
+  await page.locator('input[type="email"]').fill('hesam971@gmail.com');
+  await page.locator('input[type="password"]').click();
+  await page.locator('input[type="password"]').fill('123456abcd');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'Logout' }).click();
 });
