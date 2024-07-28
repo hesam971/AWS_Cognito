c0e04682d32ee8e5bfc551600c7f453c56bd0833
hesam971 <56349036+hesam971@users.noreply.github.com>
2024-07-28 12:51:51 +0200

    Update App.js

--- my-aws-app/my-app/src/App.js
+++ my-aws-app/my-app/src/App.js
@@ -1,32 +1,38 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter,Routes, Route,Navigate  } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
-
 import './App.css';
 import userpool from './userpool';
 
 function App() {
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-    let user=userpool.getCurrentUser();
-      if(user){
-        <Navigate to="/dashboard" replace />
-      }
-  },[]);
+  useEffect(() => {
+    let user = userpool.getCurrentUser();
+    if (user) {
+      navigate('/dashboard', { replace: true });
+    }
+  }, [navigate]);
 
   return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+function AppWrapper() {
+  return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/signup' element={<Signup />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-      </Routes>
+      <App />
     </BrowserRouter>
   );
 }
 
-export default App;
+export default AppWrapper;
